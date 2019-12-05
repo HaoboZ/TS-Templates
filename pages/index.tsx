f5ae4ef1096330c395bef355d84f54071a3aeae2
HaoboZ <haobozhang9081@gmail.com>
2019-12-05 08:16:19 -0800

    next

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import React from 'react';
+
+
+export default function Index() {
+	return <div>
+		<Head>
+			<title>Templates</title>
+		</Head>
+		
+		<div>Hello World!</div>
+	</div>;
+}
