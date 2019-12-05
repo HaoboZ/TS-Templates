bfcebbde15fd2a092bfd3f9d8d9b305d506c4916
HaoboZ <haobozhang9081@gmail.com>
2019-12-05 09:08:36 -0800

    expo and react native

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+
+export default function App() {
+	return <View style={{
+		flex:            1,
+		backgroundColor: '#fff',
+		alignItems:      'center',
+		justifyContent:  'center'
+	}}>
+		<Text>Hello World</Text>
+	</View>;
+}
--- server/app.ts
+++
@@ -1,6 +0,0 @@
-import * as path from 'path';
-
-
-global[ '__basedir' ] = path.join( __dirname, '../client' );
-
-require( './src/main' );
