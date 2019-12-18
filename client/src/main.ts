dbea8563c954f2b8c3949193ef5710eb08363350
HaoboZ <haobozhang9081@gmail.com>
2019-12-18 10:38:12 -0800

    better controls

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+import Controls from './controls';
 
 
 export default class Main extends THREE.Scene {
@@ -9,7 +10,9 @@ export default class Main extends THREE.Scene {
 	// WebGL renderer with anti-aliasing
 	renderer = new THREE.WebGLRenderer( { antialias: true } );
 	
-	controls: OrbitControls;
+	controls: Controls;
+	
+	updateList = [];
 	
 	constructor() {
 		super();
@@ -28,25 +31,26 @@ export default class Main extends THREE.Scene {
 		
 		this.renderer.setAnimationLoop( () => {
 			this.update();
+			this.updateList.forEach( item => item.update() );
 			this.renderer.render( this, this.camera );
 		} );
 	}
 	
 	create() {
+		this.controls = new Controls( this.camera, this.renderer.domElement );
+		this.addUpdate( this.controls );
+		
 		this.camera.position.set( 0, 0, 10 );
 		
-		this.controls = new OrbitControls( this.camera, this.renderer.domElement );
-		this.controls.update();
-		
-		// this.add( new THREE.AxesHelper() );
+		this.add( new THREE.AxesHelper( 1 ) );
+		this.add( new THREE.AmbientLight( '#ffffff' ) );
 		
 		const loader = new THREE.FontLoader();
 		loader.load( 'https://raw.githubusercontent.com/mrdoob/three.js/dev/examples/fonts/helvetiker_regular.typeface.json', ( font ) => {
 			const textGeometry = new THREE.TextGeometry( 'Hello World!', {
 				      font,
-				      size:          1,
-				      height:        .2,
-				      curveSegments: 12
+				      size:   1,
+				      height: .2
 			      } ),
 			      textMaterial = new THREE.MeshBasicMaterial( { color: '#00ff00' } );
 			
@@ -64,7 +68,21 @@ export default class Main extends THREE.Scene {
 	}
 	
 	update() {
-		this.controls.update();
+	}
+	
+	addUpdate( item ) {
+		if ( !item.update ) return;
+		const index = this.updateList.indexOf( item );
+		if ( index === -1 ) {
+			this.updateList.push( item );
+		}
+	}
+	
+	removeUpdate( item ) {
+		const index = this.updateList.indexOf( item );
+		if ( index > -1 ) {
+			this.updateList.splice( index, 1 );
+		}
 	}
 	
 }
