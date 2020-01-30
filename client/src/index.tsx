2a77dd89e68429bab925dd6331d31c6eff8f36aa
HaoboZ <haobozhang9081@gmail.com>
2020-01-30 09:28:03 -0800

    scale reference changed

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -11,9 +11,9 @@ export function Game() {
 			pixelArt: true
 		},
 		scale:   {
-			mode:       Phaser.Scale.FIT,
+			mode:       Phaser.Scale.ScaleModes.FIT,
 			parent:     'root',
-			autoCenter: Phaser.Scale.CENTER_BOTH,
+			autoCenter: Phaser.Scale.Center.CENTER_BOTH,
 			width:      1280,
 			height:     720
 		},
